a86a0af5efd76e271e86ad7063259efa689091d1
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-06 16:19:03 -0400

    added framework for server.js

---
+++ develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+
+// Setting up express
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing and to read static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+// API routes
+require("./routes/apiRoutes")(app);
+
+// HTML routes
+require("./routes/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
+
