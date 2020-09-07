eaa0522673202c58b489885dda6ca7a1e990cda7
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-07 15:14:24 -0400

    rerouting

--- apiroutes.js
+++ apiroutes.js
@@ -6,7 +6,7 @@ function getNotes() {
 
     let notes = JSON.parse(data);
 
-    // Give each note an ID that matches its index (this gets run for every time the page is refreshed)
+    // Give each note an ID that matches its index
     for (let i = 0; i < notes.length; i++) {
         notes[i].id = '' + i;
     }
